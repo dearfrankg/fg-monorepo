9bf1c3305d378905b052088a9f55d4a1b8e772a5
Frank Gutierrez <dearfrankg@gmail.com>
2022-11-19 03:00:32 -0800

    update

--- rtl/rtl-antd/components/select/__tests__/select.spec.js
+++ rtl/rtl-antd/components/select/__tests__/select.spec.js
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, within, fireEvent, act } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import DemoSelect from "../select";
 
