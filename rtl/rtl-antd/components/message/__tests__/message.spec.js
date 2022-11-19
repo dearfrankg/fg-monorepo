f2064eb88174353ddead42b72a8e798568cf6170
Frank Gutierrez <dearfrankg@gmail.com>
2022-11-19 05:44:02 -0800

    improved message test

--- rtl/rtl-antd/components/message/__tests__/message.spec.js
+++ rtl/rtl-antd/components/message/__tests__/message.spec.js
@@ -1,28 +1,14 @@
-import { act, render, screen, fireEvent, waitFor } from "@testing-library/react";
-import { message } from "antd";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import DemoMessage from "..";
 
 describe("<DemoMessage />", () => {
-  beforeEach(() => {
-    jest.useFakeTimers();
-  });
-
-  afterEach(() => {
-    jest.useRealTimers();
-
-    act(() => {
-      message.destroy();
-    });
-  });
-
   it("", async () => {
     render(<DemoMessage />);
 
     fireEvent.click(screen.getByRole("button"));
 
-    let message;
     await waitFor(() => {
-      [message] = Array.from(document.querySelectorAll(".ant-message-notice"));
+      const [message] = Array.from(document.querySelectorAll(".ant-message-notice"));
       expect(message).toHaveTextContent(/we have liftoff/i);
     });
   });
