fc7efb9fed09b7623e467732a685ac48874f3416
Frank Gutierrez <dearfrankg@gmail.com>
2022-12-05 01:26:28 -0800

    next-auth working

--- react-applied/next-auth/src/pages/_app.js
+++ react-applied/next-auth/src/pages/_app.js
@@ -1,25 +1,23 @@
-// import { SessionProvider } from "next-auth/react";
-// import { ChakraProvider, Grid } from "@chakra-ui/react";
+import { SessionProvider } from "next-auth/react";
+import { ChakraProvider, Grid } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  return <Component {...pageProps} />;
-
-  // return (
-  //   <SessionProvider session={session}>
-  //     <ChakraProvider>
-  //       <Grid
-  //         sx={{
-  //           h: "100vh",
-  //           px: "5rem",
-  //           placeItems: "center",
-  //           textAlign: "center",
-  //         }}
-  //       >
-  //         <Component {...pageProps} />
-  //       </Grid>
-  //     </ChakraProvider>
-  //   </SessionProvider>
-  // );
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider>
+        <Grid
+          sx={{
+            h: "100vh",
+            px: "5rem",
+            placeItems: "center",
+            textAlign: "center",
+          }}
+        >
+          <Component {...pageProps} />
+        </Grid>
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
--- react-applied/next-auth/src/pages/index.js
+++ react-applied/next-auth/src/pages/index.js
@@ -1,39 +1,37 @@
-// import React from "react";
-// import { useRouter } from "next/router";
-// import { Heading, Button, Grid } from "@chakra-ui/react";
-// import { useSession, signIn, signOut } from "next-auth/react";
+import React from "react";
+import { useRouter } from "next/router";
+import { Heading, Button, Grid } from "@chakra-ui/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home = () => {
-  // const { data: session } = useSession();
+  const { data: session } = useSession();
 
-  // const { push, asPath } = useRouter();
+  const { push, asPath } = useRouter();
 
-  // const handleSignOut = async () => {
-  //   const data = await signOut({ redirect: false, callbackUrl: "/auth/signin" });
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false, callbackUrl: "/auth/signin" });
 
-  //   push(data.url);
-  // };
+    push(data.url);
+  };
 
-  // const handleSignIn = () => push(`/auth/signin?callbackUrl=${asPath}`);
+  const handleSignIn = () => push(`/auth/signin?callbackUrl=${asPath}`);
 
-  return <h1>hello</h1>;
+  return (
+    <Grid placeItems="center" gridRowGap="1rem">
+      {session ? (
+        <>
+          <Heading>Signed in as {session.user.email}</Heading>
+          <Button onClick={handleSignOut}>Sign out</Button>
+        </>
+      ) : (
+        <>
+          <Heading>You are not signed in</Heading>
 
-  // return (
-  //   <Grid placeItems="center" gridRowGap="1rem">
-  //     {session ? (
-  //       <>
-  //         <Heading>Signed in as {session.user.email}</Heading>
-  //         <Button onClick={handleSignOut}>Sign out</Button>
-  //       </>
-  //     ) : (
-  //       <>
-  //         <Heading>You are not signed in</Heading>
-
-  //         <Button onClick={handleSignIn}>Sign in</Button>
-  //       </>
-  //     )}
-  //   </Grid>
-  // );
+          <Button onClick={handleSignIn}>Sign in</Button>
+        </>
+      )}
+    </Grid>
+  );
 };
 
 export default Home;
