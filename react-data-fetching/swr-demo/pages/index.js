ed088bbf6dc87ae5a87ef337bc503b09b82430c1
Frank Gutierrez <dearfrankg@gmail.com>
2023-01-09 02:48:29 -0800

    update

--- react-data-fetching/swr-demo/pages/index.js
+++ react-data-fetching/swr-demo/pages/index.js
@@ -1,30 +1,44 @@
 import useSwr from "swr";
 import Link from "next/link";
-const delay = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
 
 const resources = {
+  delay: (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms)),
+
+  randomOneToTen: Math.floor(Math.random() * 10) + 1,
+
   fetcher: (url) =>
     fetch(url).then(async (res) => {
       await delay(1000);
+
+      if (randomOneToTen > 8) {
+        throw error;
+      }
+
       return res.json();
     }),
 
   components: {
-    Error: ({ isError }) => {
-      if (!isError) {
-        return null;
-      }
+    Header: ({ title }) => (
+      <header>
+        <h1>{title}</h1>
+      </header>
+    ),
 
-      return <div>Failed to load users</div>;
+    Blurb: ({ blurb }) => {
+      const lines = blurb.split(/\n\n/);
+
+      return (
+        <div style={{ background: "wheat", padding: 30, lineHeight: "2rem" }}>
+          {lines.map((line, index) => (
+            <p key={index}>{line}</p>
+          ))}
+        </div>
+      );
     },
 
-    Loading: ({ isLoading }) => {
-      if (!isLoading) {
-        return null;
-      }
+    Error: () => <div>Failed to load users</div>,
 
-      return <div>Loading...</div>;
-    },
+    Loading: () => <div>Loading...</div>,
 
     UserList: ({ users }) => {
       if (!users) {
@@ -50,25 +64,44 @@ const resources = {
   },
 };
 
-const { fetcher } = resources;
-const { Error, Loading, UserList } = resources.components;
+const { delay, randomOneToTen, fetcher } = resources;
+const { Header, Blurb, Error, Loading, UserList } = resources.components;
 
 export default function Index() {
   const { data, error } = useSwr("/api/users", fetcher);
 
-  const componentProps = {
-    isError: error,
+  const show = {
+    isError: !!error,
     isLoading: !error && !data,
-    users: data,
+    isLoaded: !error && data,
   };
 
+  const blurb = `
+This is a swr demo.
+swr is a data fetching package that makes things more convenient.
+In this demo the fetch call is mocked with a one second delay to see the loading message.
+Reload the page to randomly see the components (Error, Loading, UserList).
+
+I noticed when I click on a user and hit the back button the old data
+is still there and I get no loading notice even though it's loading.
+I looked it up online and it says that is expected behavior.
+
+I tried to write a handler to reset data when you navigate away but it did not work.  This seems to be an issue.
+`;
+
   return (
-    <>
-      <Error {...componentProps} />
+    <main style={{ margin: "auto", maxWidth: 800 }}>
+      <Header title={"swr demo"} />
 
-      <Loading {...componentProps} />
+      <Blurb blurb={blurb} />
 
-      <UserList {...componentProps} />
-    </>
+      <div style={{ padding: 30 }}>
+        {show.isError && <Error />}
+
+        {show.isLoading && <Loading />}
+
+        {show.isLoaded && <UserList {...{ users: data }} />}
+      </div>
+    </main>
   );
 }
